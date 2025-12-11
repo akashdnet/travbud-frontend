228d43da405288d629341e41130714d16365e958
akashdnet <khondokerakashmi@gmail.com>
2025-12-12 02:10:10 +0600

    vercel issue 1

--- app/(protected)/dashboard/all-trip-management/page.tsx
+++ app/(protected)/dashboard/all-trip-management/page.tsx
@@ -1,3 +1,5 @@
+import Spinner from "@/components/ui/spinner";
+import { Suspense } from "react";
 import AllTripManagementClient from "./AllTripManagementClient";
 
 
@@ -7,7 +9,9 @@ export default function AllTripManagementPage() {
             <h1 className="text-3xl font-bold text-gray-800 border-b pb-3">
                 All Trip Management
             </h1>
-            <AllTripManagementClient />
+            <Suspense fallback={<div className="flex justify-center p-10"><Spinner /></div>}>
+                <AllTripManagementClient />
+            </Suspense>
         </section>
     );
 }
